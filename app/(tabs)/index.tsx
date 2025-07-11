e9e91c9a14cf754b961829386dac1b92d67aee39
Raj <rajbaidyanath76@gmail.com>
2025-07-12 04:37:31 +0530

    routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-
-import { View, Text, StyleSheet } from 'react-native';
+import {Link} from 'expo-router'
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to My App</Text>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.textButton}>Press Me</Text>
+      </TouchableOpacity>
+      <Link href = {'/about'}>Go to about</Link>
     </View>
   );
 }
@@ -14,10 +18,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    color: 'black',
+    height: 50,
+    width: 200,
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  textButton: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
